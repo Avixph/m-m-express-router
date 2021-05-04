f181542c746bc7623eaf71aff1c54b932c63c7d2
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-05-03 20:08:44 -0400

    test

--- db/index.js
+++ db/index.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 let MONGODB_URI =
   process.env.PROD_MONGODB ||
   process.env.MONGODB_URI ||
-  "mongodb://127.0.0.1:27017/plantsDatabase";
+  `mongodb+srv://Avixph:${process.env.DB_Cluster_Password}@cluster0.6vj5i.mongodb.net/plantsDatabase?retryWrites=true&w=majority&ssl=true`;
 
 mongoose
   .connect(MONGODB_URI, {
